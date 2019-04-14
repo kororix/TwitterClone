9461c621c68d89b3cbfa2d5b19899ad2e171fec4
Ayaka Kawai <consultation.pharmacist6@gmail.com>
2019-04-14 21:14:08 +0900

    createアクションを追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,4 +5,15 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to new_tweet_path
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
 end
