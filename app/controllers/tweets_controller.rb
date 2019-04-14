1cc2398b75eb66e3ef46d65aa1446ba3e51b252a
Ayaka Kawai <consultation.pharmacist6@gmail.com>
2019-04-14 21:52:37 +0900

    つぶやき編集画面を作成（＋共通処理の作成）

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
   def index
     @tweets = Tweet.all
   end
@@ -17,12 +19,26 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "つぶやきを編集しました！"
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:content)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
